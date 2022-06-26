980b8be498ac163622f277d75b09c31929adb10f
DanielVieira12 <107599009+DanielVieira12@users.noreply.github.com>
2022-06-25 22:38:48 -0300

    commit

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$bdhHost= 'Localhost';
+$bdUsername= 'root';
+$password= '';
+$dbName= 'db_sistema';
+
+$conexao = new mysqli($bdhHost,$bdUsername,$password,$dbName);
+
+//if($conexao->connect_errno){
+    echo "Erro";
+//}
+//else{
+    echo " aeee mano deu certo";
+//}
+
+
+
+?>
